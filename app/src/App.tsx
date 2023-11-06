3bbca38691b1a5aba2dec00f35e958d6baed29aa
haejunejung <jhj9422@unist.ac.kr>
2023-11-07 00:06:16 +0900

    tailwindcss 설치

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
 import { loadWasm } from "./wasmLoader";
 
 function App() {
@@ -19,24 +17,7 @@ function App() {
     }
   }, [wasmLoaded]);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
